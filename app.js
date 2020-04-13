67358e0c57da9affaaec3a9c197dff39062cd3b1
appsius <ilyas.ustunkaya234434@gmail.com>
2020-04-13 16:10:19 +0300

    Reading Input Data

--- app.js
+++ app.js
@@ -5,22 +5,43 @@ const budgetController = (function () {
 
 // UI CONTROLLER
 const UIController = (function () {
-	//
+	const DOMStrings = {
+		inputType: '.add__type',
+		inputDescription: '.add__description',
+		inputValue: '.add__value',
+		inputBtn: '.add__btn',
+	};
+
+	return {
+		getInput: function () {
+			return {
+				type: document.querySelector(DOMStrings.inputType).value,
+				description: document.querySelector(DOMStrings.inputDescription).value,
+				value: document.querySelector(DOMStrings.inputValue).value,
+			};
+		},
+		getDOMStrings: function () {
+			return DOMStrings;
+		},
+	};
 })();
 
 // GLOBAL APP CONTROLLER
 const controller = (function (budgetCtrl, UICtrl) {
+	const DOM = UICtrl.getDOMStrings();
+
 	const ctrlAddItem = function () {
 		// get the field input data
+		const input = UICtrl.getInput();
+		console.log(input);
+
 		// add the item to budget controller
 		// add the item to UI
 		// calculate budget
 		// display budget on UI
-
-		console.log('It works...');
 	};
 
-	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
 	document.addEventListener('keypress', function (e) {
 		if (e.keyCode === 13 || e.which === 13) {
