68cd6efe27a429cfc0863d95806306f4604352bc
appsius <ilyas.ustunkaya234434@gmail.com>
2020-04-15 18:37:18 +0300

    Finishing touches: more UX

--- app.js
+++ app.js
@@ -170,6 +170,12 @@ const UIController = (function () {
 		return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
 	};
 
+	const nodeListForEach = function (list, callback) {
+		for (let i = 0; i < list.length; i++) {
+			callback(list[i], i);
+		}
+	};
+
 	return {
 		getInput: function () {
 			return {
@@ -253,12 +259,6 @@ const UIController = (function () {
 		displayPercentages: function (percentages) {
 			const fields = document.querySelectorAll(DOMStrings.expensesPercLabel);
 
-			const nodeListForEach = function (list, callback) {
-				for (let i = 0; i < list.length; i++) {
-					callback(list[i], i);
-				}
-			};
-
 			nodeListForEach(fields, function (current, index) {
 				if (percentages[index] > 0) {
 					current.textContent = percentages[index] + '%';
@@ -305,6 +305,22 @@ const UIController = (function () {
 				days[day] + ' ' + months[month] + ' ' + year;
 		},
 
+		changedType: function () {
+			const fields = document.querySelectorAll(
+				DOMStrings.inputType +
+					',' +
+					DOMStrings.inputDescription +
+					',' +
+					DOMStrings.inputValue
+			);
+
+			nodeListForEach(fields, function (cur) {
+				cur.classList.toggle('red-focus');
+			});
+
+			document.querySelector(DOMStrings.inputBtn).classList.toggle('red');
+		},
+
 		getDOMStrings: function () {
 			return DOMStrings;
 		},
@@ -325,6 +341,9 @@ const controller = (function (budgetCtrl, UICtrl) {
 		document
 			.querySelector(DOM.container)
 			.addEventListener('click', ctrlDeleteItem);
+		document
+			.querySelector(DOM.inputType)
+			.addEventListener('change', UICtrl.changedType);
 	};
 
 	const updateBudget = function () {
